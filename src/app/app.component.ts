e3c147db6f2096b5d1bf3523fafe7ffac1ba726c
yalda mobargha <mobarghayalda@gmail.com>
2024-12-15 17:09:05 +0100

    Aggiunto un esempio di card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatSliderModule,MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/prova/prova.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prova',
+  imports: [],
+  standalone: true,
+  templateUrl: './prova.component.html',
+  styleUrl: './prova.component.css'
+})
+export class ProvaComponent {
+
+}
